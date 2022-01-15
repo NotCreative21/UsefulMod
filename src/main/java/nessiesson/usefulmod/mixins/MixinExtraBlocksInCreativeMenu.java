c071814b56dc975096481a22137d7fcffcbae667
NotCreative21 <seanray410@gmail.com>
2022-01-15 16:44:13 -0500

    add slimeblock to creative redstone menu

--- src/main/java/nessiesson/usefulmod/mixins/MixinExtraBlocksInCreativeMenu.java
+++ src/main/java/nessiesson/usefulmod/mixins/MixinExtraBlocksInCreativeMenu.java
@@ -5,6 +5,7 @@ import net.minecraft.block.BlockBarrier;
 import net.minecraft.block.BlockCommandBlock;
 import net.minecraft.block.BlockDragonEgg;
 import net.minecraft.block.BlockStructure;
+import net.minecraft.block.BlockSlime;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin({BlockBarrier.class, BlockCommandBlock.class, BlockDragonEgg.class, BlockStructure.class})
+@Mixin({BlockBarrier.class, BlockCommandBlock.class, BlockDragonEgg.class, BlockSlime.class, BlockStructure.class})
 public abstract class MixinExtraBlocksInCreativeMenu extends Block {
 	protected MixinExtraBlocksInCreativeMenu(Material material) {
 		super(material);
